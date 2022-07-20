430399779dccd0d9ca0d6dc2dd49b4360c5defc4
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-07-20 12:58:31 +0530

    Add files via upload

---
+++ numpy_math.py
@@ -0,0 +1,29 @@
+import numpy as np
+# a = np.array([[9,8,7],
+#              [4,5,6]])
+# b = np.array([[11,12,13],
+#              [14,15,16]])
+# print(a+b)        # [[20 20 20]
+                    #  [18 20 22]]
+# print(a-b)        # [[ -2  -4  -6]
+                    #  [-10 -10 -10]]
+# print(a*b)        # [[99 96 91]
+                    #  [56 75 96]]
+           
+# a=np.eye(2)
+# b=np.eye(2,dtype=int)
+# c=np.eye(2,dtype=bool)
+# print(a)      # [[1. 0.]
+                # [0. 1.]]
+# print(b)     # [[1 0]
+               # [0 1]]
+# print(c)      # [[ True False]           
+                # [False  True]]
+                
+x=np.array ([[2,4,6],
+            [1,3,5]])
+y=x.astype('float')
+# print(y)   # [[2. 4. 6.] 
+           # [1. 3. 5.]]
+         
+
